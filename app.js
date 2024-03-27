5afd4cab099a44df9b269d7be37ae7bfcc4c6b87
KAbbasi006 <abbasikomal6@gmail.com>
2024-03-28 00:47:14 +0500

    assignment Q7, Q8 and Q9

---
+++ app.js
@@ -0,0 +1,15 @@
+//Q7. Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+console.log("4 + 4  = ", 4 + 4); //Addition
+console.log("4 * 2 = ", 4 * 2); //Multiplication
+console.log("10 - 2 = ", 10 - 2); //Subtraction
+console.log("32 / 4  = ", 32 / 4); //Division
+//Q8. You should create four lines that look like this:
+// console.log(5 + 3)
+// Your output should simply be four lines with the number 8 appearing once on each line.
+console.log(6 + 2); //Addition
+console.log(4 * 2); //Subtraction
+console.log(10 - 2); //Multiplication
+console.log(16 / 2); //Division
+//Q9. Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+var fav_Num = 9; //my favourite numver
+console.log("My favourite number is ".concat(fav_Num, ".")); // message that reveals my favourite number
